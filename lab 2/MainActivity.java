f3ef1104f27985319ea01f4cebed6bc69618a3ac
miriampopeti <popetimiriam@yahoo.com>
2026-05-14 00:25:16 +0300

    Add files via upload

---
+++ lab 2/MainActivity.java
@@ -0,0 +1,17 @@
+package cg.helloworld;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new HelloWorldRenderer());
+        setContentView(view);
+    }
+}
